85fe0a767d018070e3b2bb9e8c74bea8b4841df6
rickypag <ricky.pagliuca@gmail.com>
2020-06-05 17:02:22 +0200

    Merge conflict resolved

--- backend/src/api.js
+++ backend/src/api.js
@@ -6,7 +6,7 @@ const helmet     = require('helmet');
 
 const middlewares = require('./middleware/middlewares');
 
-//const auth  = require('./routes/auth');
+const auth  = require('./routes/auth');
 const activities = require('./routes/activities');
 const users = require('./routes/users');
 
@@ -31,7 +31,7 @@ api.get('/', (req, res) => {
 });
 
 // API routes
-// api.use('/auth'  , auth);
+api.use('/auth'  , auth);
 api.use('/activities', activities);
 api.use('/users', users);
 
