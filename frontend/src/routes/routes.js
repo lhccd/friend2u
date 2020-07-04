eacb72a97c05959798a099857f713a662231f216
Lorenz Dang <lorenz.dang@tum.de>
2020-07-04 16:32:55 +0200

    resolved routes merge conflict

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,6 +1,8 @@
 import { HomepageView } from "../views/HomepageView";
 import {UserLoginView} from "../views/UserLoginView";
-
+import { UserSignupView } from "../views/UserSignupView";
+import { ModeratorView } from "../views/ModeratorView";
+import {authSplashScreen} from '../authSplashScreen';
 import {ReportUserView} from "../views/ReportUserView";
 import {ReportActivityView} from "../views/ReportActivityView"
 export const routes = [
@@ -8,4 +10,9 @@ export const routes = [
     {component: UserLoginView, path: '/login'},
     {component: ReportUserView, path: '/report_user'},
     {component: ReportActivityView, path: '/report_activity'},
+    {component: authSplashScreen(UserLoginView), path: '/login', exact: true},
+    {component: authSplashScreen(UserSignupView), path: '/register', exact: true},
+    {component: authSplashScreen(HomepageView), path: '/', exact: true},
+    {component: authSplashScreen(ModeratorView), path: '/moderator'}
+
 ];
