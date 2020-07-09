82f759db363bfed46546d7d83e7d90b480154318
Hendrik <hendrik.98@web.de>
2020-07-09 11:49:23 +0200

    A few more changes; Joining and UNJoining of activities now possible and visible to the user!

--- frontend/src/components/ActivityListRow.js
+++ frontend/src/components/ActivityListRow.js
@@ -1,14 +1,15 @@
 "use strict";
 
 import React from 'react';
-import { TableRow, TableColumn, FontIcon, Button, DataTable, TableHeader, TableBody } from 'react-md';
+import { TableRow, TableColumn, FontIcon, DataTable, TableHeader, TableBody } from 'react-md';
 import { Link } from 'react-router-dom';
+import { Button, Card , Row, Col, Container, Badge } from 'react-bootstrap';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import HttpService from '../services/HttpService';
 
 //import { SimpleLink } from './SimpleLink';
 
-//import UserService from '../services/UserService';
+import UserService from '../services/AuthService';
 
 
 export class ActivityListRow extends React.Component {
@@ -17,7 +18,8 @@ export class ActivityListRow extends React.Component {
         super(props);
         this.state = {
             address: "",
-            first: true
+            first: true,
+            userID: UserService.getCurrentUser().id
         }
 
         this.setPriceSymbols = this.setPriceSymbols.bind(this);
@@ -90,7 +92,7 @@ export class ActivityListRow extends React.Component {
                                     Category: {this.props.activity.category}
                                 </TableColumn>
                                 <TableColumn width={500}>
-                                    Activityname: {this.props.activity.activityName}
+                                    Activityname: {this.props.activity.activityName} {(this.state.userID==this.props.activity.creator) ? <Badge className="badge-info">Created by you</Badge> : ""}
                                 </TableColumn>
                                 <TableColumn width={150}>
                                     Date&Time: {new Date(this.props.activity.dateTime).toLocaleString()}
--- frontend/src/views/ActivityDetailedView.js
+++ frontend/src/views/ActivityDetailedView.js
@@ -42,13 +42,31 @@ export class ActivityDetailedView extends React.Component {
         });
     }
 
+    joinUser(activityID) {
+        ActivityService.joinUser(activityID).then((message) => {
+            console.log("User has joined "+activityID)
+        }).catch((e) => {
+            console.log("Sth. went wrong with the join...")
+            console.log(e)
+        })
+    }
+
+    unJoinUser(activityID) {
+        ActivityService.unJoinUser(activityID).then((message) => {
+            console.log("User has UNjoined "+activityID)
+        }).catch((e) => {
+            console.log("Sth. went wrong with the UNjoin...")
+            console.log(e)
+        })
+    }
+
     render() {
         if (this.state.loading) {
             return (<h2>Loading the selected activity...</h2>);
         }
 
         return (
-            <ActivityDetail activity={this.state.activity} onDelete={(id) => this.deleteActivity(id)}/>
+            <ActivityDetail activity={this.state.activity} onJoin={(activityID) => this.joinUser(activityID)} onUNJoin={(activityID) => this.unJoinUser(activityID)} onDelete={(id) => this.deleteActivity(id)}/>
         );
     }
 }
