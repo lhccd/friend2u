7fe454b18815bed1ab0f62794ec47af2cdda0fdf
rickypag <ricky.pagliuca@gmail.com>
2020-07-18 16:54:39 +0200

    cors

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 
 //Configuration variables for our backend.
 //const backend_address = process.env.BACKEND_ADDRESS || 'http://localhost:3000';
-const backend_address = 'http://localhost:3000';
+const backend_address = 'http://127.0.0.1:3000';
 
 module.exports = {
 	backend_address
