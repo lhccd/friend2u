22801821b6762ec77065e9a1c22ada122ad80cdf
Lorenz Dang <lorenz.dang@tum.de>
2020-07-18 15:05:07 +0200

    added impressum to the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import {FaInstagramSquare as Ig, FaTwitterSquare as Tw, FaFacebookSquare as Fb, FaLinkedin as Ln} from 'react-icons/fa'
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Popover, OverlayTrigger, Button} from 'react-bootstrap';
+
+const popover = (
+    <Popover id="popover-basic">
+        <Popover.Title as="h3">Impressum</Popover.Title>
+        <Popover.Content>
+            <strong>Friend2U GmbH</strong><br/><br/>
+            Boltzmannstraße 3<br/>
+            85748 Garching bei München<br/><br/><br/>
+            <p><strong>Contact</strong><br/>
+                E-Mail: friend2u@gmail.com<br/><br/>
+
+                <strong>Management Board</strong><br/>
+                Riccardo Pagliuca, Hendrik Bothe, Zeyu Zhou & Lorenz Dang<br/><br/>
+
+                <strong>Responsible for Content</strong><br/>
+                Riccardo Pagliuca, Hendrik Bothe, Zeyu Zhou & Lorenz Dang<br/><br/>
+
+                The pictures on our website were used from the following sources:<br/>
+                <ul>
+                    <li>Getty Images</li>
+                    <li>iStock</li>
+                    <li>LifeArt Images</li>
+                    <li>Flickr</li>
+                </ul>
+                </p>
+        </Popover.Content>
+    </Popover>
+);
+
+const Impressum = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+        <Button variant="light">Impressum</Button>
+    </OverlayTrigger>
+);
 
 export class Footer extends React.Component {
 
@@ -11,8 +45,14 @@ export class Footer extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Container fluid style={{backgroundColor:"#333", color:"#fff", fontSize:"30px", marginTop:"0px", marginBottom:"0", padding:"auto", bottom: "0", position: "flexible"}}>
+                <Container fluid style={{backgroundColor:"#333", color:"#fff", fontSize:"30px", marginTop:"0px", marginBottom:"0", padding: "10px", bottom: "0", position: "flexible"}}>
                     <Row className="justify-content-around text-center">
+                        <Col>
+                            <h1 style={{color:"white"}}>Friend2U</h1>
+                        </Col>
+                        <Col>
+                            <Impressum/>
+                        </Col>
                         <Col>
                             <Ig/>
                         </Col>
